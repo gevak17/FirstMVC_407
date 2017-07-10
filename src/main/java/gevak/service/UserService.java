82b75508dcd648e9cdc4e358543d47cb49eefe9f
gevak17 <go31512@bigmir.net>
2017-07-10 21:39:13 +0300

    UserEnd

--- src/main/java/gevak/service/UserService.java
+++ src/main/java/gevak/service/UserService.java
@@ -1,7 +1,21 @@
 package gevak.service;
 
-/**
- * Created by Oleh on 09.07.2017.
- */
-public interface UserService  {
+import gevak.entity.User;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+public interface UserService {
+
+    void save(User user);
+
+    void save(String name, String surName);
+
+    void save(String name, String surName, int p1, int p2, int p3, int atestat);
+
+    User findByOne(int id);
+
+    List<User> findByAll();
+
 }
